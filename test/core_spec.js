d8c239802ca430b38a0600c6523bb4d462e07930
Ricky Valencia <3d.rickyV@gmail.com>
2016-08-19 15:58:12 -0700

    start core testing

---
+++ test/core_spec.js
@@ -0,0 +1,17 @@
+import { Map } from 'immutable';
+import { expect } from 'chai';
+
+import { setEntries } from '..src/core';
+
+describe('application logic', () => {
+  describe('setEntries function', () => {
+    it('adds the entries to the state', () => {
+      const state = new Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(new Map({
+        entries: ['Trainspotting', '28 Days Later'],
+      }));
+    });
+  });
+});
