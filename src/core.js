367a0f976f0be13c576ebf427c42b19c8c97f443
Ricky Valencia <3d.rickyV@gmail.com>
2016-08-19 17:00:21 -0700

    implement next function

--- src/core.js
+++ src/core.js
@@ -1,5 +1,13 @@
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 export function setEntries (state, entries) {
   return state.set('entries', new List(entries));
+}
+
+export function next (state) {
+  const entries = stage.get('entries');
+  return state.merge({
+    vote: new Map({ pair: entires.take(2) }),
+    entries: entries.skip(2)
+  });
 }
