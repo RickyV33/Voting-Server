210bf4ec916b0e5005e9e869ecd334f174ede511
Ricky Valencia <3d.rickyV@gmail.com>
2016-09-01 00:45:35 -0700

    implement getWinners function

--- src/core.js
+++ src/core.js
@@ -15,3 +15,21 @@ export function next (state) {
 export function vote (state, entry) {
   return state.updateIn([ 'vote', 'tally', entry ], 0, tally => tally + 1);
 }
+
+function getWinners (vote) {
+  if (!vote) {
+    return [];
+  }
+  const [ a, b ] = vote.get('pair');
+  const aVotes = vote.getIn([ 'tally', a ], 0);
+  const bVotes = vote.getIn([ 'tally', b ], 0);
+  if (aVotes > bVotes) {
+    return [ a ];
+  }
+  else if (aVotes < bVotes) {
+    return [ b ];
+  }
+  else {
+    return [ a, b ]
+  }
+}
