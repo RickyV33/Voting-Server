00e0693cd67696a2769e3b3b5e36afcfd7a852e4
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-09 23:16:22 -0700

    add test for VOTE action

--- test/reducer.js
+++ test/reducer.js
@@ -6,10 +6,10 @@ import { expect } from 'chai';
 import reducer from '../src/reducer';
 
 describe('reducer module', () => {
-  it('handles SET_ENTRIES', () => {
+  it('handles SET_ENTRIES action', () => {
     const initialState = Map();
     const action = {
-      type:    'SET_ENTRIES',
+      type: 'SET_ENTRIES',
       entries: ['Trainspotting']
     };
     const nextState = reducer(initialState, action);
@@ -19,16 +19,37 @@ describe('reducer module', () => {
     }));
   });
 
-  it ('handles NEXT', () => {
+  it('handles NEXT action', () => {
     const initialState = fromJS({
       entries: ['Trainspotting', '28 Days Later']
     });
-    const action = {type: 'NEXT'};
+    const action = { type: 'NEXT' };
     const nextState = reducer(initialState, action);
 
     expect(nextState).to.equal(fromJS({
       vote: {
         pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    }));
+  });
+
+  it('handles VOTE action', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    });
+    const action = {
+      type: 'VOTE',
+      entry: 'Trainspotting'
+    };
+    const nextState = reducer(initialState, action);
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: { 'Trainspotting': 1 }
       },
       entries: []
     }));
